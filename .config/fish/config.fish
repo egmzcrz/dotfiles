41cfaf4212646224cc222c0d8c30f810ffcde6a8
enrique <enriquegomezcruz@gmail.com>
2024-02-10 22:54:10 -0600

    add new alias dotfiles vimrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,13 +14,15 @@ fzf_configure_bindings --directory=\ct
 alias so='source ~/.config/fish/config.fish'
 
 # bin aliases
-alias vim='nvim --clean -u /Users/henry/.config/nvimbasic/init.lua'
+alias vim='nvim --clean -u /Users/henry/.config/nvim-minimal/init.lua'
 alias python='python3'
 alias rc='radix-calc'
 alias ll='exa --long --icons'
 
 # config files
-alias vimrc='cd ~/.config/nvim/ && explore'
+alias dotfiles='cd ~/dotfiles/ && explore'
+alias nvimrc='cd ~/.config/nvim/ && explore'
+alias vimrc='vim ~/.config/nvim-minimal/init.lua'
 alias fishrc='nvim ~/.config/fish/config.fish'
 alias kittyrc='nvim ~/.config/kitty/kitty.conf'
 
@@ -68,7 +70,7 @@ end
 ###########
 # Exports #
 ###########
-export FZF_DEFAULT_COMMAND="fd --type f --type d . \$dir"
+export FZF_DEFAULT_COMMAND="fd -H --type f --type d . \$dir"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
