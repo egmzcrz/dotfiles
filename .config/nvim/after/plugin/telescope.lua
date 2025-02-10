b84d2ec785bb708a698621e2be628c692d6726f5
enrique <enriquegomezcruz@gmail.com>
2025-02-10 13:11:50 -0600

    update keymappings for telescopge

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -16,22 +16,17 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        previewer = true,
-    })
-end, { desc = '[/] Fuzzily search in current buffer' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope error diagnostics' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>st',
+vim.keymap.set('n', '<leader>fB', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find,
+    { desc = 'Telescope fuzzy search in current buffer' })
+
+vim.keymap.set('n', '<leader>fT', builtin.live_grep, { desc = 'Telescope text search' })
+vim.keymap.set('n', '<leader>ft',
     function()
         require('telescope.builtin').grep_string({ only_sort_text = true, search = '' })
-    end, { desc = '[S]earch [T]ext' })
+    end, { desc = 'Telescope fuzzy text search' })
--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,3 +1,3 @@
--- Toggleterm
+-- ToggleTerm
 vim.keymap.set({ "n", "t" }, "<C-s>", "<cmd>ToggleTerm direction=float<CR>")
 vim.keymap.set("t", "<C-n>", "<C-\\><C-n>")
