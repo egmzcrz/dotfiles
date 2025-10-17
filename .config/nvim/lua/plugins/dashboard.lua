a3c3ef00c25c1150d36236203d2096b66dbfa5b6
enrique <enriquegomezcruz@gmail.com>
2025-10-17 11:17:07 -0600

    add dashboard config file

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,27 @@
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      -- dashboard configuration
+      width = 60,
+      row = nil, -- dashboard position. nil for center
+      col = nil, -- dashboard position. nil for center
+      preset = {
+        header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+      },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
