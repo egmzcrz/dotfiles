06b6e286ff490949e880dd0cb9d2153c995e8ec4
Enrique Gómez Cruz <enriquegomezcruz@gmail.com>
2026-05-17 13:29:29 -0600

    add ghostty multiplexing keybindings

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 SRC_DIR="$(pwd)"
 
 TARGET_FILES=(
-	".vimrc"
-	".vim"
-	".editrc"
-	".inputrc"
-	".fdignore"
-	".config/nvim-minimal"
-	".config/nvim"
-	".config/fish"
-	".config/doom"
-	".config/vifm"
-	".config/kitty"
-	".config/ghostty"
-	".config/wezterm"
+  ".vimrc"
+  ".vim"
+  ".editrc"
+  ".inputrc"
+  ".fdignore"
+  ".config/nvim-minimal"
+  ".config/nvim"
+  ".config/fish"
+  ".config/doom"
+  ".config/vifm"
+  ".config/kitty"
+  ".config/ghostty"
+  ".config/wezterm"
 )
 
-for dotfile in "${TARGET_FILES[@]}";do
-	rm -rf "${HOME}/${dotfile}"
-	ln -svf "${SRC_DIR}/${dotfile}" "${HOME}/${dotfile}"
+for dotfile in "${TARGET_FILES[@]}"; do
+  rm -rf "${HOME}/${dotfile}"
+  ln -svf "${SRC_DIR}/${dotfile}" "${HOME}/${dotfile}"
 done
